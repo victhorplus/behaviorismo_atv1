c7b430c72dd3fd3d6fcfb5f8925b60db57395b36
Victhor Souza <victhor.souza@dellead.com>
2022-05-01 16:26:59 -0300

    refact: Muda estilo de rolagem e lógica scroll do menu lateral

--- src/app/components/nav-home/nav-home.component.ts
+++ src/app/components/nav-home/nav-home.component.ts
@@ -22,23 +22,22 @@ export class NavHomeComponent implements OnInit {
     this.team = false;
   }
   
-  scrollToSection(value: number){
-    let sectionSize = window.innerHeight;
-    window.scroll(0 , sectionSize * (value-1));
+  scrollToSection(section: string){
+    window.location.href = window.location.origin + "/" + section;
   }
 
   scrollListener(){
     let sectionSize = window.innerHeight;
     window.onscroll = (e: any) => {
-      if(window.scrollY*1.5 <= sectionSize){
+      if(window.scrollY*1.2 <= sectionSize){
         this.clearActive();
         this.home = true;
       }
-      else if(window.scrollY*1.5 >= sectionSize && window.scrollY*1.5 <= sectionSize*2){ 
+      else if(window.scrollY*1.2 >= sectionSize && window.scrollY*1.2 <= sectionSize*2){ 
         this.clearActive();
         this.team = true;
       }
-      else if(window.scrollY*1.5 >= sectionSize*2){
+      else if(window.scrollY*1.2 >= sectionSize*2){
         this.clearActive();
         this.content = true;
       }
